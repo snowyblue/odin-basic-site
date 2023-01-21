01c8c57e0603c44b28ab29f65eb60f59bb274266
Amie Wei <amie.wei12@gmail.com>
2023-01-21 13:08:23 -0600

    remove node_modules folder and add nav links

--- index.js
+++ index.js
@@ -9,39 +9,27 @@ const port = 3000;
 const server = http.createServer( async (req, res) => {
     let data;
     try {
-        if (req.url === '/' || req.url === "/home") {
+        if (req.url === '/' || req.url === "/home" || req.url === "/index.html") {
             data = await fs.readFile('./index.html', 'utf8');
-        } else if (req.url === '/about') {
+            res.writeHeader(200, { "Content-Type": "text/html" });
+        } else if (req.url === '/about' || req.url === "/about.html") {
             data = await fs.readFile('./about.html', 'utf8');
-        } else if (req.url === '/contact-me') {
+            res.writeHeader(200, { "Content-Type": "text/html" });
+        } else if (req.url === '/contact-me' || req.url === "/contact-me.html") {
             data = await fs.readFile('./contact-me.html', 'utf8');
+            res.writeHeader(200, { "Content-Type": "text/html" });
         } else {
             data = await fs.readFile('./404.html', 'utf8');
+            res.writeHeader(404, { "Content-Type": "text/html" });
         }
     } catch(error) {
         console.log(error)
     } finally {
-        res.writeHeader(200, { "Content-Type": "text/html" });
+        console.log(res)
         res.write(data)
         res.end();
     }
-
 });
-
-async function displayIndex() {
-    const fileName = './index.html';
-    try {
-      const newData = await fs.readFile(fileName, 'utf8');
-      return newData
-
-    //   await fs.writeFile( fileName, content, err => {} );
-      console.log(newData);
-    } catch (err) {
-        console.log('error caught')
-      console.log(err);
-    }
-  }
-
 
 
 server.listen(port, hostname, () => {
